d29e40951f8ca77a608d11b3a6ebbcd23e7b937a
hex7c0 <carniellifrancesco@gmail.com>
2014-05-29 12:39:10 +0200

    1.0.2

--- examples/app.js
+++ examples/app.js
@@ -3,9 +3,9 @@
  * example with express
  * 
  * @package browser-language
- * @subpackage lib
+ * @subpackage example
  * @version 0.0.1
- * @author hex7c0 <0x7c0@teboss.tk>
+ * @author hex7c0 <hex7c0@gmail.com>
  * @license GPLv3
  */
 
@@ -14,10 +14,8 @@
  */
 // import
 try {
-    var app = require('express')();
-    var cookie = require('cookie-parser');
     var language = require('../index.js'); // use 'browser-language' instead
-    app.use(cookie('pippo'));
+    var app = require('express')();
 } catch (MODULE_NOT_FOUND) {
     console.log(MODULE_NOT_FOUND);
     process.exit(1);
@@ -25,14 +23,13 @@ try {
 
 // using middleware
 app.use(language({
-    cookie : 'new_cookie_name'
+    cookie: 'new_cookie_name',
 }))
 
 // express routing
-app.get('/', function(req, res) {
+app.get('/',function(req,res) {
     res.send('hello world!');
 });
-
 // server starting
 app.listen(3000);
 console.log('starting server on port 3000');
--- index.js
+++ index.js
@@ -1,191 +1,266 @@
 "use strict";
 /**
- * browser-language
- * 
- * @package browser-language
- * @subpackage index
- * @version 1.0.1
- * @author hex7c0 <0x7c0@teboss.tk>
- * @license GPLv3
- * @overview main module
+ * @file browser-language main
+ * @module browser-language
+ * @version 1.0.2
+ * @author hex7c0 <hex7c0@gmail.com>
  * @copyright hex7c0 2014
+ * @license GPLv3
  */
 
-// global
-var done = false;
-var languageAll = {
-    'ab' : 'Abkhazian',
-    'af' : 'Afrikaans',
-    'an' : 'Aragonese',
-    'ar' : 'Arabic',
-    'as' : 'Assamese',
-    'az' : 'Azerbaijani',
-    'be' : 'Belarusian',
-    'bg' : 'Bulgarian',
-    'bn' : 'Bengali',
-    'bo' : 'Tibetan',
-    'br' : 'Breton',
-    'bs' : 'Bosnian',
-    'ca' : 'Catalan',
-    'ce' : 'Chechen',
-    'co' : 'Corsican',
-    'cs' : 'Czech',
-    'cu' : 'Church',
-    'cy' : 'Welsh',
-    'da' : 'Danish',
-    'de' : 'German',
-    'el' : 'Greek',
-    'en' : 'English',
-    'eo' : 'Esperanto',
-    'es' : 'Spanish',
-    'et' : 'Estonian',
-    'eu' : 'Basque',
-    'fa' : 'Persian',
-    'fi' : 'Finnish',
-    'fj' : 'Fijian',
-    'fo' : 'Faroese',
-    'fr' : 'French',
-    'fy' : 'Frisian',
-    'ga' : 'Irish',
-    'gd' : 'Gaelic',
-    'gl' : 'Galician',
-    'gv' : 'Manx',
-    'he' : 'Hebrew',
-    'hi' : 'Hindi',
-    'hr' : 'Croatian',
-    'ht' : 'Haitian',
-    'hu' : 'Hungarian',
-    'hy' : 'Armenian',
-    'id' : 'Indonesian',
-    'is' : 'Icelandic',
-    'it' : 'Italian',
-    'ja' : 'Japanese',
-    'jv' : 'Javanese',
-    'ka' : 'Georgian',
-    'kg' : 'Kongo',
-    'ko' : 'Korean',
-    'ku' : 'Kurdish',
-    'kw' : 'Cornish',
-    'ky' : 'Kirghiz',
-    'la' : 'Latin',
-    'lb' : 'Luxembourgish',
-    'li' : 'Limburgan',
-    'ln' : 'Lingala',
-    'lt' : 'Lithuanian',
-    'lv' : 'Latvian',
-    'mg' : 'Malagasy',
-    'mk' : 'Macedonian',
-    'mn' : 'Mongolian',
-    'mo' : 'Moldavian',
-    'ms' : 'Malay',
-    'mt' : 'Maltese',
-    'my' : 'Burmese',
-    'nb' : 'Norwegian',
-    'ne' : 'Nepali',
-    'nl' : 'Dutch',
-    'nn' : 'Norwegian',
-    'no' : 'Norwegian',
-    'oc' : 'Occitan',
-    'pl' : 'Polish',
-    'pt' : 'Portuguese',
-    'rm' : 'Raeto-Romance',
-    'ro' : 'Romanian',
-    'ru' : 'Russian',
-    'sc' : 'Sardinian',
-    'se' : 'Sami',
-    'sk' : 'Slovak',
-    'sl' : 'Slovenian',
-    'so' : 'Somali',
-    'sq' : 'Albanian',
-    'sr' : 'Serbian',
-    'sv' : 'Swedish',
-    'sw' : 'Swahili',
-    'tk' : 'Turkmen',
-    'tr' : 'Turkish',
-    'ty' : 'Tahitian',
-    'uk' : 'Ukrainian',
-    'ur' : 'Urdu',
-    'uz' : 'Uzbek',
-    'vi' : 'Vietnamese',
-    'vo' : 'Volapuk',
-    'yi' : 'Yiddish',
-    'zh' : 'Chinese'
+// variables
+/**
+ * @global
+ */
+var my = {}, LANG = {}, languageAll = {
+    'ab': 'Abkhazian',
+    'af': 'Afrikaans',
+    'an': 'Aragonese',
+    'ar': 'Arabic',
+    'as': 'Assamese',
+    'az': 'Azerbaijani',
+    'be': 'Belarusian',
+    'bg': 'Bulgarian',
+    'bn': 'Bengali',
+    'bo': 'Tibetan',
+    'br': 'Breton',
+    'bs': 'Bosnian',
+    'ca': 'Catalan',
+    'ce': 'Chechen',
+    'co': 'Corsican',
+    'cs': 'Czech',
+    'cu': 'Church',
+    'cy': 'Welsh',
+    'da': 'Danish',
+    'de': 'German',
+    'el': 'Greek',
+    'en': 'English',
+    'eo': 'Esperanto',
+    'es': 'Spanish',
+    'et': 'Estonian',
+    'eu': 'Basque',
+    'fa': 'Persian',
+    'fi': 'Finnish',
+    'fj': 'Fijian',
+    'fo': 'Faroese',
+    'fr': 'French',
+    'fy': 'Frisian',
+    'ga': 'Irish',
+    'gd': 'Gaelic',
+    'gl': 'Galician',
+    'gv': 'Manx',
+    'he': 'Hebrew',
+    'hi': 'Hindi',
+    'hr': 'Croatian',
+    'ht': 'Haitian',
+    'hu': 'Hungarian',
+    'hy': 'Armenian',
+    'id': 'Indonesian',
+    'is': 'Icelandic',
+    'it': 'Italian',
+    'ja': 'Japanese',
+    'jv': 'Javanese',
+    'ka': 'Georgian',
+    'kg': 'Kongo',
+    'ko': 'Korean',
+    'ku': 'Kurdish',
+    'kw': 'Cornish',
+    'ky': 'Kirghiz',
+    'la': 'Latin',
+    'lb': 'Luxembourgish',
+    'li': 'Limburgan',
+    'ln': 'Lingala',
+    'lt': 'Lithuanian',
+    'lv': 'Latvian',
+    'mg': 'Malagasy',
+    'mk': 'Macedonian',
+    'mn': 'Mongolian',
+    'mo': 'Moldavian',
+    'ms': 'Malay',
+    'mt': 'Maltese',
+    'my': 'Burmese',
+    'nb': 'Norwegian',
+    'ne': 'Nepali',
+    'nl': 'Dutch',
+    'nn': 'Norwegian',
+    'no': 'Norwegian',
+    'oc': 'Occitan',
+    'pl': 'Polish',
+    'pt': 'Portuguese',
+    'rm': 'Raeto-Romance',
+    'ro': 'Romanian',
+    'ru': 'Russian',
+    'sc': 'Sardinian',
+    'se': 'Sami',
+    'sk': 'Slovak',
+    'sl': 'Slovenian',
+    'so': 'Somali',
+    'sq': 'Albanian',
+    'sr': 'Serbian',
+    'sv': 'Swedish',
+    'sw': 'Swahili',
+    'tk': 'Turkmen',
+    'tr': 'Turkish',
+    'ty': 'Tahitian',
+    'uk': 'Ukrainian',
+    'ur': 'Urdu',
+    'uz': 'Uzbek',
+    'vi': 'Vietnamese',
+    'vo': 'Volapuk',
+    'yi': 'Yiddish',
+    'zh': 'Chinese'
 };
 
+/**
+ * set language to cookie
+ * 
+ * @function normal
+ * @param {Object} res - response to client
+ * @param {String} lang - string for cookie
+ * @param {Boolean} signed - if cookie'll be signed
+ * @return
+ */
+function set(res,lang,signed) {
+
+    res.cookie(my.cookie,lang,{
+        domain: my.domain,
+        path: my.path,
+        maxAge: my.age,
+        httpOnly: my.httpOnly,
+        secure: my.secure,
+        signed: signed,
+    });
+    return;
+
+}
+/**
+ * detect language and (if accepted) build it. Store information on normal cookie
+ * 
+ * @function normal
+ * @param {Object} req - client request
+ * @param {Object} res - response to client
+ * @param {next} next - continue routes
+ * @return
+ */
+function normal(req,res,next) {
+
+    var done = false;
+    if (req.cookies == undefined) {
+        req.cookies = {};
+    }
+    if (req.cookies[my.cookie] == undefined) {
+        // send only one cookie
+        if (req.headers['accept-language']) {
+            // fix with benchmark script
+            var languagesTyp = req.headers['accept-language'].replace(/q=[0-9.]*[,]?/g,'').split(
+                    ';');
+            for (var i = 0, il = languagesTyp.length; i < il; i++) {
+                var language = languagesTyp[i].substring(0,2);
+                if (LANG[language]) {
+                    set(res,language,false);
+                    req.cookies[my.cookie] = language;
+                    done = true;
+                    break;
+                }
+            }
+        }
+    } else if (LANG[req.cookies[my.cookie]]) {
+        // check right cookie
+        done = true;
+    }
+
+    if (done == false) {
+        // reset
+        set(res,LANG._default,false);
+        req.cookies[my.cookie] = LANG._default;
+    }
+    return next();
+
+}
+/**
+ * detect language and (if accepted) build it. Store information on signed cookie
+ * 
+ * @function signed
+ * @param {Object} req - client request
+ * @param {Object} res - response to client
+ * @param {next} next - continue routes
+ * @return
+ */
+function signed(req,res,next) {
+
+    var done = false;
+    if (req.signedCookies == undefined) {
+        req.signedCookies = {};
+    }
+    if (req.signedCookies[my.cookie] == undefined) {
+        // send only one cookie
+        if (req.headers['accept-language']) {
+            // fix with benchmark script
+            var languagesTyp = req.headers['accept-language'].replace(/q=[0-9.]*[,]?/g,'').split(
+                    ';');
+            for (var i = 0, il = languagesTyp.length; i < il; i++) {
+                var language = languagesTyp[i].substring(0,2);
+                if (LANG[language]) {
+                    set(res,language,true);
+                    req.signedCookies[my.cookie] = language;
+                    done = true;
+                    break;
+                }
+            }
+        }
+    } else if (LANG[req.signedCookies[my.cookie]]) {
+        // check right cookie
+        done = true;
+    }
+
+    if (done == false) {
+        // reset
+        set(res,LANG._default,true);
+        req.signedCookies[my.cookie] = LANG._default;
+    }
+    return next();
+
+}
+/**
+ * setting options
+ * 
+ * @function language
+ * @param {Object} options: various options. check README.md
+ * @return {function}
+ */
 function language(options) {
-    /**
-     * setting options
-     * 
-     * @param object options: various options. check README.md
-     * @return function
-     */
 
     var include = __dirname + '/lib/dictionary.js';
     var options = options || {};
-    var LANG = options.dictionary || require(include).LANG;
-    options.cookie = options.cookie || 'lang';
-    options.age = parseInt(options.age) || 3600 * 1000 * 24 * 365;
-
-    if (LANG._default == undefined) {
-        // force
-        LANG = require(include).LANG;
-    } else if (!languageAll[LANG._default]) {
-        console.error('language misconfigured');
-        LANG = require(include).LANG;
+    var lang = options.dictionary || require(include).LANG;
+    my = {
+        cookie: String(options.cookie || 'lang'),
+        domain: options.domain || null,
+        path: String(options.path || '/'),
+        age: parseInt(options.age) || 1000 * 3600 * 24 * 365,
+        httpOnly: Boolean(options.httpOnly),
+        secure: Boolean(options.secure),
     }
-    process.env.LANG = LANG._default;
+    if (lang._default == undefined) {
+        // force
+        lang = require(include).LANG;
+    } else if (!languageAll[lang._default]) {
+        console.error('language misconfigured');
+        lang = require(include).LANG;
+    }
+    process.env.lang = lang._default;
+    LANG = lang;
+    if (Boolean(options.signed)) {
+        return signed;
+    } else {
+        return normal;
+    }
 
-    return function detection(req, res, next) {
-        /**
-         * Detect language and (if accepted) build it. Store information on
-         * cookie
-         * 
-         * @param object req: request
-         * @param object res: response
-         * @param object next: continue routes
-         * @return function
-         */
-
-        if (req.cookies[options.cookie] == undefined) {
-            // not already set
-            if (res._headers == undefined) {
-                // send only one cookie
-                if (req.headers['accept-language']) {
-                    // fix with benchmark script
-                    var languagesTyp = req.headers['accept-language'].replace(
-                            /q=[0-9.]*[,]?/g, '').split(';');
-
-                    for (var i = 0; i < languagesTyp.length; i++) {
-                        var language = languagesTyp[i].substring(0, 2);
-                        if (LANG[language]) {
-                            res.cookie(options.cookie, language, {
-                                maxAge : options.age,
-                            });
-                            req.cookies[options.cookie] = language;
-                            done = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        } else if (LANG[req.cookies[options.cookie]]) {
-            // check right cookie
-            done = true;
-        }
-
-        if (done == false) {
-            // reset
-            res.cookie(options.cookie, LANG._default, {
-                maxAge : options.age,
-            });
-            req.cookies[options.cookie] = LANG._default;
-        }
-
-        return next();
-    };
 };
 
 /**
  * exports function
+ * 
+ * @exports language
  */
-exports = module.exports = language;
+module.exports = language;
