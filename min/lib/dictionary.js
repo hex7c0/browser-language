02afca2bee2a44125679b8998e7de8fa00140985
hex7c0 <carniellifrancesco@gmail.com>
2014-09-28 15:08:53 +0200

    grunt

--- min/lib/dictionary.js
+++ min/lib/dictionary.js
@@ -1,6 +1,1 @@
-/*
- * browser-language v1.2.7
- * (c) hex7c0 http://supergiovane.tk/#/browser-language
- * Licensed under GPLv3
- */
 "use strict";module.exports.LANG=Object.create(null,{_default:{enumerable:!0,writable:!1,configurable:!1,value:"en"},en:{enumerable:!0,writable:!1,configurable:!1,value:"en"},it:{enumerable:!0,writable:!1,configurable:!1,value:"it"},de:{enumerable:!0,writable:!1,configurable:!1,value:"de"},fr:{enumerable:!0,writable:!1,configurable:!1,value:"fr"}});
